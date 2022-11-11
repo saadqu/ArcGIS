4cddc2e5bcaf1ecb477f1d473f1f02fa89909f04
Saad <saad.qu01@gmail.com>
2022-11-11 16:35:50 +0500

    [app-module]: added map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EsriMapComponent } from './components/esri-map/esri-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EsriMapComponent
   ],
   imports: [
     BrowserModule,
